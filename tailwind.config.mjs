881cdd7c7e4169c5c07c183ae86818f0ee1f5ad4
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-04-17 01:57:53 -0400

    feat: Add Hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Roboto', 'Helvetica', 'sans-serif'],
+		},
+		colors: {
+			'huella-green': 'rgba(234,253,238,0.9)',
+			'huella-text-green': '#1E7B5C',
+			'huella-text-gray': '#575757',
+		},
 		extend: {},
 	},
 	plugins: [
